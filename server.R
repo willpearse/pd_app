da97bf9590885ddf127f029f2fab5d894e051d9d
Will Pearse <will.pearse@gmail.com>
2017-03-20 11:52:55 -0600

    Remove SESs as unecessary (constant source pool) and confusing

--- server.R
+++ server.R
@@ -19,7 +19,7 @@ function(input, output) {
             output$comparative.comm <- renderText(
                 paste0("Dataset details:", "\n", paste(capture.output(print(c.data)), collapse="\t\n"))
             )
-            return(data.frame(.pd(c.data), MPD=.mpd(c.data), MNTD=.mntd(c.data), SESmpd=.ses.mpd(c.data), SESmntd=.ses.mntd(c.data)))
+            return(data.frame(.pd(c.data), MPD=.mpd(c.data), MNTD=.mntd(c.data)))
         }
     })
 }
